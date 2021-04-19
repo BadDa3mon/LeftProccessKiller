d863fe007541935a2aab446a9b326706d1b5e866
BadDa3mon <pasha.padero@gmail.com>
2021-04-20 01:46:58 +0300

    Добавьте файлы проекта.

---
+++ LeftProccessKiller/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace LeftProccessKiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.kill_button = new System.Windows.Forms.Button();
+            this.process_title = new System.Windows.Forms.Label();
+            this.listview_process = new System.Windows.Forms.ListView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.select_file_button = new System.Windows.Forms.Button();
+            this.killall_button = new System.Windows.Forms.Button();
+            this.processes_path_label = new System.Windows.Forms.Label();
+            this.k_proccesses = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // kill_button
+            // 
+            this.kill_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.kill_button.Location = new System.Drawing.Point(375, 117);
+            this.kill_button.Name = "kill_button";
+            this.kill_button.Size = new System.Drawing.Size(125, 36);
+            this.kill_button.TabIndex = 0;
+            this.kill_button.Text = "Убить";
+            this.kill_button.UseVisualStyleBackColor = true;
+            this.kill_button.Click += new System.EventHandler(this.kill_button_Click);
+            // 
+            // process_title
+            // 
+            this.process_title.AutoSize = true;
+            this.process_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.process_title.Location = new System.Drawing.Point(12, 9);
+            this.process_title.Name = "process_title";
+            this.process_title.Size = new System.Drawing.Size(134, 29);
+            this.process_title.TabIndex = 1;
+            this.process_title.Text = "Процессы:";
+            // 
+            // listview_process
+            // 
+            this.listview_process.HideSelection = false;
+            this.listview_process.Location = new System.Drawing.Point(17, 42);
+            this.listview_process.Name = "listview_process";
+            this.listview_process.Size = new System.Drawing.Size(352, 153);
+            this.listview_process.TabIndex = 2;
+            this.listview_process.UseCompatibleStateImageBehavior = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(17, 220);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(146, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Импорт из файла";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // select_file_button
+            // 
+            this.select_file_button.BackColor = System.Drawing.Color.Transparent;
+            this.select_file_button.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.select_file_button.FlatAppearance.BorderSize = 0;
+            this.select_file_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.select_file_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.select_file_button.Location = new System.Drawing.Point(162, 206);
+            this.select_file_button.Margin = new System.Windows.Forms.Padding(0);
+            this.select_file_button.Name = "select_file_button";
+            this.select_file_button.Size = new System.Drawing.Size(40, 45);
+            this.select_file_button.TabIndex = 4;
+            this.select_file_button.Text = "✓";
+            this.select_file_button.UseVisualStyleBackColor = false;
+            this.select_file_button.Visible = false;
+            this.select_file_button.Click += new System.EventHandler(this.select_file_button_Click);
+            // 
+            // killall_button
+            // 
+            this.killall_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.killall_button.Location = new System.Drawing.Point(375, 159);
+            this.killall_button.Name = "killall_button";
+            this.killall_button.Size = new System.Drawing.Size(125, 36);
+            this.killall_button.TabIndex = 6;
+            this.killall_button.Text = "Убить все";
+            this.killall_button.UseVisualStyleBackColor = true;
+            this.killall_button.Click += new System.EventHandler(this.killall_button_Click);
+            // 
+            // processes_path_label
+            // 
+            this.processes_path_label.AutoSize = true;
+            this.processes_path_label.Location = new System.Drawing.Point(14, 198);
+            this.processes_path_label.Name = "processes_path_label";
+            this.processes_path_label.Size = new System.Drawing.Size(78, 17);
+            this.processes_path_label.TabIndex = 7;
+            this.processes_path_label.Text = "Процессы:";
+            // 
+            // k_proccesses
+            // 
+            this.k_proccesses.AutoSize = true;
+            this.k_proccesses.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.k_proccesses.Location = new System.Drawing.Point(145, 9);
+            this.k_proccesses.Name = "k_proccesses";
+            this.k_proccesses.Size = new System.Drawing.Size(26, 29);
+            this.k_proccesses.TabIndex = 8;
+            this.k_proccesses.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 255);
+            this.Controls.Add(this.k_proccesses);
+            this.Controls.Add(this.processes_path_label);
+            this.Controls.Add(this.killall_button);
+            this.Controls.Add(this.select_file_button);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listview_process);
+            this.Controls.Add(this.process_title);
+            this.Controls.Add(this.kill_button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ProccessKiller";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button kill_button;
+        private System.Windows.Forms.Label process_title;
+        private System.Windows.Forms.ListView listview_process;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button select_file_button;
+        private System.Windows.Forms.Button killall_button;
+        private System.Windows.Forms.Label processes_path_label;
+        private System.Windows.Forms.Label k_proccesses;
+    }
+}
+
---
+++ LeftProccessKiller/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace LeftProccessKiller
+{
+    public partial class Form1 : Form
+    {
+        public OpenFileDialog OFD;
+        public SaveFileDialog SFD;
+        public string fileName, configName;
+        public bool ofConfig, showError, showAddInfo, isSavedPath;
+        public string[] processesForKill;
+        public Form1()
+        {
+            InitializeComponent();
+            openConfigurationIfNeeded();
+            OFD = new OpenFileDialog();
+            SFD = new SaveFileDialog();
+            OFD.Filter = "Текстовые документы(*.txt)|*.txt|Все файлы(*.*)|*.*";
+            SFD.Filter = "Текстовые документы(*.txt)|*.txt|Все файлы(*.*)|*.*";
+        }
+
+        public void openConfigurationIfNeeded()
+        {
+            configName = "da3mon_conf.cfg";
+            ofConfig = System.IO.File.Exists(configName);
+            if (ofConfig != true)
+            {
+                MessageBox.Show("Error! Check da3mon_conf.cfg in work folder!", "Error!", MessageBoxButtons.OK);
+                this.Close();
+            }
+            else 
+            {
+                getConfig();
+                ofConfig = true;
+                if (fileName.Length == 0)
+                {
+                    MessageBox.Show($"Select .txt file, i'm saved him path in config!", "Info!", MessageBoxButtons.OK);
+                    isSavedPath = false;
+                }
+                else 
+                { 
+                    System.IO.File.ReadAllText(fileName);
+                    isSavedPath = true;
+                    checkBox1.Checked = true;
+                    createItems();
+                }
+            }
+        }
+
+        private void getConfig()
+        {
+            string[] lines = System.IO.File.ReadAllLines(configName); int i = 1;
+            int firstQuote = lines[i].IndexOf('"') + 1, length = lines[i].Length - firstQuote - 2;
+            fileName = lines[i].Substring(firstQuote, length); i++;
+            firstQuote = lines[i].IndexOf('"') + 1; length = lines[i].Length - firstQuote - 2;
+            showError = bool.Parse(lines[i].Substring(firstQuote, length)); i++;
+            firstQuote = lines[i].IndexOf('"') + 1; length = lines[i].Length - firstQuote - 2;
+            showAddInfo = bool.Parse(lines[i].Substring(firstQuote, length));
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            bool isChecked = checkBox1.Checked;
+            select_file_button.Visible = isChecked;
+        }
+
+        private void select_file_button_Click(object sender, EventArgs e)
+        {
+            if (OFD.ShowDialog() == DialogResult.Cancel) { return; }
+            if (fileName.Length == 0) { fileName = OFD.FileName; }
+            createItems();
+        }
+
+        private void createItems()
+        {
+            string proccesses = System.IO.File.ReadAllText(fileName);
+            processesForKill = proccesses.Split(';');
+            for (int i = 0; i < processesForKill.Length; i++)
+            {
+                try
+                {
+                    Process proc = Process.GetProcessesByName(processesForKill[i])[0];
+                    if (proc != null)
+                    {
+                        ListViewItem item = new ListViewItem();
+                        item.Text = processesForKill[i];
+                        listview_process.Items.Add(item);
+                    }
+                }
+                catch (IndexOutOfRangeException IndexEx)
+                {
+                    if (showError) { MessageBox.Show($"Error! Proccess with name {processesForKill[i]} not exist!", "Error!", MessageBoxButtons.OK); }
+                }
+            }
+            k_proccesses.Text = listview_process.Items.Count.ToString();
+            updateElements();
+        }
+
+        private void updateElements()
+        {
+            processes_path_label.Text = $"Процессы: {fileName}";
+        }
+
+        private void kill_button_Click(object sender, EventArgs e)
+        {
+            killSelectedProccesses(listview_process.SelectedItems);
+        }
+
+        private void killSelectedProccesses(ListView.SelectedListViewItemCollection Selected)
+        {
+            if (Selected.Count != 0)
+            {
+                for (int i = 0; i < Selected.Count; i++)
+                {
+                    Process proc = Process.GetProcessesByName(Selected[i].Text)[0];
+                    proc.Kill();
+                    listview_process.Items.RemoveByKey(Selected[i].Text);
+                    listview_process.Update();
+                }
+            }
+            else { noActiveProccess(); }
+        }
+
+        private void killall_button_Click(object sender, EventArgs e)
+        {
+            killAllProccesses(listview_process.Items);
+            createItems();
+        }
+
+        private void resetConfig()
+        {
+            string first = "#BadDa3mon CONF for ProcessKiller\n";
+            string second = "filePath=" + "\u0022" + $"{fileName}" + "\u0022;\n";
+            string third = "showError=" + "\u0022" + $"{showError.ToString()}" + "\u0022;\n";
+            string fourth = "showAddInfo=" + "\u0022" + $"{showAddInfo.ToString()}" + "\u0022;";
+            string all = first + second + third + fourth;
+            System.IO.File.WriteAllText(configName, all);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            resetConfig();
+        }
+
+        private void killAllProccesses(ListView.ListViewItemCollection allItems)
+        {
+            int N = allItems.Count;
+            if (allItems.Count != 0)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    Process proc = Process.GetProcessesByName(allItems[0].Text)[0];
+                    proc.Kill();
+                    listview_process.Items.RemoveAt(0);
+                    if (showAddInfo) { MessageBox.Show($"Proccess with name {allItems[0].Text} removed", "Info!", MessageBoxButtons.OK); }
+                }
+            }
+            else { noActiveProccess(); }
+            createItems();
+        }
+
+        private void noActiveProccess()
+        {
+            MessageBox.Show($"Error! No active processes!", "Error!", MessageBoxButtons.OK);
+        }
+    }
+}
